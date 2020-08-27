f03c3a0652bc4aaf374e5e75141139d5df41a67e
Ciwan Clifford <theciwan@gmail.com>
2020-08-27 11:19:52 +0200

    Can now perform one calculation at a time while saving the result

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,19 +1,59 @@
-const buttons = Array.from(document.getElementById("button-holder").children);
 const screenInput = document.getElementById("screen-input");
-buttons.forEach(button => button.addEventListener("click", () => button.textContent !="C" ? enterInput(button.textContent) : clear()));
+const buttons = Array.from(document.getElementById("button-holder").children);
+const numberButtons = (buttons.filter((button)=>button.className=="int-btn"?true:false));
+const operatorButtons = (buttons.filter((button)=>button.className=="op-btn"?true:false));
+const clearButton = buttons.find(button=>button.className=="clear-btn");
+const equalButton = buttons.find(button=>button.className=="equal-btn");
+numberButtons.forEach(button => button.addEventListener("click", () => displayInput(button.textContent)));
+operatorButtons.forEach(button => button.addEventListener("click", () => operatorPressed(button.textContent)));
+clearButton.addEventListener("click", clearInput);
+equalButton.addEventListener("click", evaluate);
+let calculationValues = [];
 
+function displayInput(sym){
 
-function enterInput(sym){
     screenInput.textContent+=sym;
 }
 
-function clear(){
+function clearInput(){
     screenInput.textContent = "";
+    calculationValues = [];
+}
+function operatorPressed(operator){
+    calculationValues.push(parseInt(screenInput.textContent));
+    calculationValues.push(operator);
+    screenInput.textContent = "";
+}
+
+function evaluate(){
+    calculationValues.push(parseInt(screenInput.textContent));
+    let result = 0;
+    let currentOperator = "";
+    let currentNum1 = 0;
+    let currentNum2 = 0;
+    let firstNumberAssigned = false;
+    calculationValues.forEach(value => {
+        if (!firstNumberAssigned){
+            currentNum1 = value;
+            firstNumberAssigned = true;
+        }
+        else if(typeof value != "number"){
+            currentOperator = value;
+        }
+        else{
+            currentNum2 = value;
+            result = operate(currentOperator, currentNum1, currentNum2);
+        }
+    });
+    clearInput();
+    calculationValues.push(result);
+    screenInput.textContent="";
+    displayInput(result);
 }
 
 
 function add (num1, num2){
-    return num1 + num2;
+    return num1+num2;
 }
 
 function subtract(num1, num2){
@@ -40,9 +80,9 @@ function operate(operator, num1, num2){
         case "-":
             return subtract(num1,num2);
         case "*":
-            return multiply(num1, num2);
+            return multiply(num1,num2);
         case "/":
-            return divide(num1/num2);
+            return divide(num1,num2);
         default:
             return "Invalid operation";
     }
