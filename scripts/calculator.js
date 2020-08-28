6b938e7211dee167aca44b2c152cddc51f0442bc
Ciwan Clifford <theciwan@gmail.com>
2020-08-28 07:50:01 +0200

    Added keyboard support, fixed a bug with the input and changed up the styling. The 0/0 bug persists, but other than that the only thing missing is an undo button and this project is finished.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,19 +1,37 @@
 const screenInput = document.getElementById("screen-input");
 const buttons = Array.from(document.getElementById("button-holder").children);
 const numberButtons = (buttons.filter((button)=>button.className=="int-btn"?true:false));
+const numberButtonKeyboardKeys = numberButtons.map((button)=>button.textContent);
 const operatorButtons = (buttons.filter((button)=>button.className=="op-btn"?true:false));
+const operatorButtonKeyboardKeys = operatorButtons.map((button)=>button.textContent);
 const clearButton = buttons.find(button=>button.className=="clear-btn");
 const equalButton = buttons.find(button=>button.className=="equal-btn");
 numberButtons.forEach(button => button.addEventListener("click", () => displayInput(button.textContent)));
 operatorButtons.forEach(button => button.addEventListener("click", () => operatorPressed(button.textContent)));
 clearButton.addEventListener("click", clearInput);
-equalButton.addEventListener("click", evaluate);
+equalButton.addEventListener("click", ()=>evaluate(true));
 
 let lastButtonWasOperator = false;
 let calculationMemory = [];
 let screenString = "0";
 let lastScreenString = "0";
-let snarkyMessage = "Criminal!";
+let snarkyMessage = "That is Illegal!";
+
+document.onkeydown = (e) => {
+    if (numberButtonKeyboardKeys.find((key) => key == e.key)){
+        displayInput(e.key);
+    }
+    else if (operatorButtonKeyboardKeys.find((key) => key == e.key)){
+        operatorPressed(e.key);
+    }
+    else if (e.key === "Escape"){
+        clearInput();
+    }
+
+    else if (e.key === "Enter"){
+        evaluate(true);
+    }
+};
 
 function displayInput(sym){
     if(sym === "."){
@@ -21,7 +39,7 @@ function displayInput(sym){
             return;
         }
     }
-    if (screenString === "0" || lastButtonWasOperator || screenString === snarkyMessage){
+    if (screenString === "0" || lastButtonWasOperator || screenString === snarkyMessage || screenString === "Infinity"){
         screenString = sym;
     }
     else{
@@ -32,20 +50,24 @@ function displayInput(sym){
 }
 
 function clearInput(){
+    lastButtonWasOperator = false;
     calculationMemory = [];
     screenString = "0";
     screenInput.textContent = "0";
 }
 function operatorPressed(operator){
-    evaluate();
+    evaluate(false);
     calculationMemory[1] = operator;
     
 
     lastButtonWasOperator = true;
 }
-function evaluate(){
+function evaluate(equalWasPressed){
     
     let result;
+    if(equalWasPressed){
+        lastButtonWasOperator = false;
+    }
     
     if (!calculationMemory[1]){
         calculationMemory[0] = parseFloat(screenString);
@@ -59,7 +81,6 @@ function evaluate(){
     }
 
     else if (calculationMemory[1] && calculationMemory[0]){
-        console.log(1);
         result = operate(calculationMemory[1], calculationMemory[0], parseFloat(screenString));
         calculationMemory[0] = result;
         calculationMemory.pop();
@@ -67,13 +88,7 @@ function evaluate(){
     
     screenString = "";
     displayInput(result);
-
-
 }
-/*
-    
-   
-*/ 
 
 function add (num1, num2){
     return num1+num2;
@@ -92,7 +107,7 @@ function divide(num1, num2){
         return snarkyMessage;
     }
     else{
-        return Math.round(((num1/num2) + Number.EPSILON) * 1000000) / 1000000;
+        return Math.round(((num1/num2) + Number.EPSILON) * 100000) / 100000;
     }
 }
 
